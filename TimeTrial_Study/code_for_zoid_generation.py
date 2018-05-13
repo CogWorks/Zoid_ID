bd931d7a1d1a2acb3299077813a2ee9210b7b999
Song Ye <yes@rpi.edu>
2018-05-13 17:19:29 -0400

    commit

---
+++ TimeTrial_Study/code_for_zoid_generation.py
@@ -0,0 +1,10 @@
+scale_factor = 1.0 #change this variable to get larger/smaller zoids displayed
+
+# code for displaying zoids
+zoid_s=((.5 * scale_factor,0),(1.5 * scale_factor,0),(1.5 * scale_factor,1 * scale_factor),(-.5 * scale_factor,1 * scale_factor),(-.5 * scale_factor,0),(-1.5 * scale_factor,0),(-1.5 * scale_factor,-1 * scale_factor),(.5 * scale_factor,-1 * scale_factor))
+zoid_z=((-.5 * scale_factor,0),(-1.5 * scale_factor,0),(-1.5 * scale_factor,1 * scale_factor),(.5 * scale_factor,1 * scale_factor),(.5 * scale_factor,0),(1.5 * scale_factor,0),(1.5 * scale_factor,-1),(-.5 * scale_factor,-1 * scale_factor))
+zoid_o=((1 * scale_factor,1 * scale_factor),(-1 * scale_factor,1 * scale_factor),(-1 * scale_factor,-1 * scale_factor),(1 * scale_factor,-1 * scale_factor))
+zoid_i=((2 * scale_factor,-.5 * scale_factor),(2 * scale_factor,.5 * scale_factor),(-2 * scale_factor,.5 * scale_factor),(-2 * scale_factor,-.5 * scale_factor))
+zoid_t=((.5 * scale_factor,0),(1.5 * scale_factor,0),(1.5 * scale_factor,1 * scale_factor),(-1.5 * scale_factor,1 * scale_factor),(-1.5 * scale_factor,0),(-.5 * scale_factor,0),(-.5 * scale_factor,-1 * scale_factor),(0.5 * scale_factor,-1 * scale_factor))
+zoid_l=((0,-.5 * scale_factor),(1 * scale_factor,-.5 * scale_factor),(1 * scale_factor,-1.5 * scale_factor),(-1 * scale_factor,-1.5 * scale_factor),(-1 * scale_factor,1.5 * scale_factor),(0,1.5 * scale_factor))
+zoid_j=((0,-.5 * scale_factor),(-1 * scale_factor,-.5 * scale_factor),(-1 * scale_factor,-1.5 * scale_factor),(1 * scale_factor,-1.5 * scale_factor),(1 * scale_factor,1.5 * scale_factor),(0,1.5 * scale_factor))
