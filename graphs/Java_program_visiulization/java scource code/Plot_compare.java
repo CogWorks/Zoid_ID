de298275d08f2429d37e79f4f72e546d9d568c36
Song Ye <yes@rpi.edu>
2018-05-13 18:39:18 -0400

    Commit

---
+++ graphs/Java_program_visiulization/java scource code/Plot_compare.java
@@ -0,0 +1,56 @@
+package zoidid_visi;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JTextField;
+
+public class Plot_compare {
+
+	private JFrame frame;
+	private JTextField txtGraph;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Plot_compare window = new Plot_compare();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Plot_compare() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		txtGraph = new JTextField();
+		txtGraph.setText("Graph 1");
+		frame.getContentPane().add(txtGraph, BorderLayout.NORTH);
+		txtGraph.setColumns(10);
+	}
+
+}
